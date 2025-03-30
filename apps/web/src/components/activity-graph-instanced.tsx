5f8049e4f772749591d9a6bd217810d5cffa8b0c
Vedant Chainani <vedantchainani1084@gmail.com>
2025-03-31 04:01:40 +0530

    chore: add instances activity graph

---
+++ apps/web/src/components/activity-graph-instanced.tsx
@@ -0,0 +1,76 @@
+import { Instance, Instances } from '@react-three/drei';
+import { MeshPhysicalMaterial } from 'three';
+import { getInstancesData, groupTrapProps } from '~/helpers';
+import { useRoundedTrapeziumGeometry } from '~/hooks';
+import type {
+  ContributionData,
+  GroupedTrapeziumInstanceProps,
+  RoundedTrapeziumInstanceProps,
+} from '~/types';
+
+export type GroupedTrapInstancesProps = {
+  traps: RoundedTrapeziumInstanceProps[];
+  smoothness?: number;
+  bevelSegments?: number;
+  steps?: number;
+  creaseAngle?: number;
+};
+
+export const ActivityGraphInstanced = ({
+  data,
+}: { data: ContributionData }) => {
+  const groups = groupTrapProps(getInstancesData(data));
+
+  return (
+    <>
+      {groups.map((group) => {
+        return (
+          <GroupInstance
+            key={group.key}
+            group={group}
+          />
+        );
+      })}
+    </>
+  );
+};
+
+interface GroupInstanceProps {
+  group: GroupedTrapeziumInstanceProps;
+}
+
+export const GroupInstance = ({ group }: GroupInstanceProps) => {
+  const { instances: groupTraps, key } = group;
+  // All traps in this group share the same args and radius.
+  // biome-ignore lint/style/noNonNullAssertion: we know it's not null
+  const { args, color } = groupTraps[0]!;
+
+  const geometry = useRoundedTrapeziumGeometry(args[1], args[2]);
+
+  return (
+    <Instances
+      geometry={geometry}
+      limit={groupTraps.length}
+      material={
+        new MeshPhysicalMaterial({
+          roughness: 0.2,
+          metalness: 0.1,
+          clearcoat: 1,
+          clearcoatRoughness: 0.1,
+          color: color,
+        })
+      }
+    >
+      {groupTraps.map((trap, i) => {
+        return (
+          <Instance
+            key={i.toString()}
+            position={trap.position}
+            rotation={[0, Math.PI / 2, 0]}
+            color={trap.color}
+          />
+        );
+      })}
+    </Instances>
+  );
+};
--- apps/web/src/types/index.ts
+++ apps/web/src/types/index.ts
@@ -1,5 +1,27 @@
-export type ActivityMatrix = {
-  rows: number;
-  columns: number;
-  data: number[][];
+export interface ContributionData {
+  totalContributions: number;
+  weeks: Week[];
+}
+
+export interface Week {
+  firstDay: string;
+  contributionDays: ContributionDay[];
+}
+
+export interface ContributionDay {
+  contributionCount: number;
+  date: string;
+  color: string;
+  weekday: number;
+}
+
+export interface RoundedTrapeziumInstanceProps {
+  args: [number, number, number, number];
+  position: [number, number, number];
+  color: string;
+}
+
+export type GroupedTrapeziumInstanceProps = {
+  key: string;
+  instances: RoundedTrapeziumInstanceProps[];
 };
