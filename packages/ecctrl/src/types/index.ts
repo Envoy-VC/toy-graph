b714a42db80c926561943754cc8236e32264e886
Vedant Chainani <vedantchainani1084@gmail.com>
2025-04-02 02:04:58 +0530

    chore: add game store

--- packages/ecctrl/src/types/index.ts
+++ packages/ecctrl/src/types/index.ts
@@ -16,3 +16,18 @@ export type UseFollowCameraProps = {
   camCollisionSpeedMult?: number;
   camListenerTarget?: CameraListenerTargetType;
 };
+
+export type AnimationSet = {
+  idle?: string;
+  walk?: string;
+  run?: string;
+  jump?: string;
+  jumpIdle?: string;
+  jumpLand?: string;
+  fall?: string;
+  // Currently support four additional animations
+  action1?: string;
+  action2?: string;
+  action3?: string;
+  action4?: string;
+};
